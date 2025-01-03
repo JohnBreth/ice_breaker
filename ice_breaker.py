a77cc6ccf911e83cc52de5f3655801c48bdca63f
John Breth <jbreth1@gmail.com>
2025-01-03 16:37:13 +0000

    Updated ice_breaker.py

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,11 +3,12 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-if __name__ == "__main__":
-    load_dotenv()
 
-    print("Hello LangChain")
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
 
     summary_template = """
     given the information {information} about a person I want you to create:
@@ -22,10 +23,13 @@ if __name__ == "__main__":
     llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
 
     chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/john-breth/"
-    )
-
+   
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Ice Breaker Enter")
+    ice_break_with(name="John Breth Architect")
